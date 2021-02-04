2922123015e83bfbfc862d9f308de8322585b584
manu-p-1 <mjp45770@uga.edu>
2021-02-04 13:23:35 -0500

    Fixed documentation

--- Ampere/Statistics/StatExceptions.cs
+++ Ampere/Statistics/StatExceptions.cs
@@ -18,7 +18,7 @@ namespace Ampere.Statistics
         /// <summary>
         /// Creates a new overloaded InsufficientDataSetException containing a message.
         /// </summary>
-        /// <param name="message"></param>
+        /// <param name="message">The message of this exception type</param>
         public InsufficientDataSetException(string message) : base(message) { }
 
         /// <summary>
@@ -39,35 +39,36 @@ namespace Ampere.Statistics
     }
 
     /// <summary>
-    /// 
+    /// An exception that is thrown when no Mode exists in the data set.
     /// </summary>
-    [Serializable()]
+    [Serializable]
     public class NoModeException : Exception
     {
         /// <summary>
-        /// 
+        /// Creates a new NoModeException.
         /// </summary>
         public NoModeException()
         { }
 
         /// <summary>
-        /// 
+        /// Creates a new overloaded NoModeException containing a message.
         /// </summary>
-        /// <param name="message"></param>
+        /// <param name="message">The message of this exception type</param>
         public NoModeException(string message) : base(message) { }
 
         /// <summary>
-        /// 
+        /// Creates a new overloaded NoModeException containing a message and an inner Exception.
         /// </summary>
-        /// <param name="message"></param>
-        /// <param name="inner"></param>
+        /// <param name="message">The message of this exception type</param>
+        /// <param name="inner">The inner Exception</param>
         public NoModeException(string message, Exception inner) : base(message, inner) { }
 
         /// <summary>
-        /// 
+        /// Creates a new overloaded NoModeException containing a <see cref="System.Runtime.Serialization.SerializationInfo"/> instance
+        /// and a <see cref="System.Runtime.Serialization.StreamingContext"/> instance.
         /// </summary>
-        /// <param name="info"></param>
-        /// <param name="context"></param>
+        /// <param name="info">The SerializationInfo instance</param>
+        /// <param name="context">The StreamingContext instance</param>
         protected NoModeException(System.Runtime.Serialization.SerializationInfo info,
             System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
