bc9a475ffc2b59091bd537bbf3d01db579e52860
Manu P <mjp45770@uga.edu>
2022-01-01 14:59:32 -0500

    Added test method for isAll property

--- AmpereMSTest/EnumerableUtilsTest.cs
+++ AmpereMSTest/EnumerableUtilsTest.cs
@@ -12,15 +12,27 @@ namespace AmpereMSTest
     public class EnumerableUtilsTest
     {
         [TestMethod]
-        public void InnerContainsTest()
+        public void InnerContainsTest_WithIsAllTrue()
         {
             int[] x = { 1, 2, 3, 4, 5, 6, 7 };
             int[] y = { 3, 4, 5 };
             int[] z = { 10, 11, 12 };
 
-            var icp = x.InnerContains(y, z);
+            var icp = x.InnerContains(true, y, z);
             Assert.AreEqual(icp.CheckContains(), false);
             Assert.AreEqual(icp.ViolatedEnumerable, z);
+        }
+
+        [TestMethod]
+        public void InnerContainsTest_WithIsAllFalse()
+        {
+            int[] x = { 1, 2, 3, 4, 5, 6, 7 };
+            int[] y = { 3, 4, 5 };
+            int[] z = { 10, 11, 12, 3 };
+
+            var icp = x.InnerContains(false, y, z);
+            Assert.AreEqual(icp.CheckContains(), true);
+            Assert.AreEqual(icp.ViolatedEnumerable, null);
         }
     }
 }
