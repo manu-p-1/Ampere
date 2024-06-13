74d662b5fb467e3df22a5e3b5d1c89fc28d3900e
Manu P <28782438+manu-p-1@users.noreply.github.com>
2024-06-13 09:41:03 -0400

    Update Shuffler.cs
    Converted to static class

--- Ampere/Base/Shuffler.cs
+++ Ampere/Base/Shuffler.cs
@@ -6,41 +6,35 @@ namespace Ampere.Base
     /// <summary>
     /// A utility to shuffle an array using the Fisher-Yates algorithm.
     /// </summary>
-    /// <typeparam name="T">The element type of the array.</typeparam>
-    internal sealed class Shuffler<T>
+    public static class Shuffler
     {
-        private readonly T[] _data;
-        private readonly RandomNumberGenerator _rng;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Shuffler{T}"/> class with the specified array.
-        /// </summary>
-        /// <param name="data">The array to be shuffled.</param>
-        /// <exception cref="ArgumentNullException">Thrown when the provided array is null.</exception>
-        public Shuffler(T[] data)
-        {
-            _data = data ?? throw new ArgumentNullException(nameof(data));
-            _rng = RandomNumberGenerator.Create();
-        }
-
         /// <summary>
         /// Shuffles the array using the Fisher-Yates algorithm with cryptographically secure random number generation.
         /// </summary>
+        /// <typeparam name="T">The element type of the array.</typeparam>
+        /// <param name="data">The array to be shuffled.</param>
         /// <returns>The shuffled array.</returns>
-        public T[] Shuffle()
+        /// <exception cref="ArgumentNullException">Thrown when the provided array is null.</exception>
+        public static T[] Shuffle<T>(T[] data)
         {
-            var len = _data.Length;
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data));
+            }
+
+            var len = data.Length;
+            var rng = RandomNumberGenerator.Create();
             var bytes = new byte[4];
 
             for (var i = len - 1; i > 0; i--)
             {
-                _rng.GetBytes(bytes);
+                rng.GetBytes(bytes);
                 var j = (int)(BitConverter.ToUInt32(bytes, 0) % (uint)(i + 1));
 
-                (_data[i], _data[j]) = (_data[j], _data[i]);
+                (data[i], data[j]) = (data[j], data[i]);
             }
 
-            return _data;
+            return data;
         }
     }
 }
